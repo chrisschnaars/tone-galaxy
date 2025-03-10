2c0f744295e44c0baa50f8f6a71a6af93e67c082
Chris Schnaars <schnaarschris@gmail.com>
2025-03-10 10:26:17 -0400

    use path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,20 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "src/assets/icons/": "./assets/icons" });
     eleventyConfig.addPassthroughCopy({ "src/css/": "./" });
     eleventyConfig.addWatchTarget('./src/js/');
-    
-    return {
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-
-        dir: {
-            input: 'src',
-            output: '_site',
-        },
-    };
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
 };
+
+export const config = {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    pathPrefix: "tone-galaxy",
+
+    dir: {
+        input: 'src',
+        output: '_site',
+    },
+}
