2a3a008a5e82c26e5959815e287985d2b936d6ad
Chris Schnaars <schnaarschris@gmail.com>
2025-03-10 09:58:16 -0400

    fix up 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
-
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "src/assets/icons/": "./assets/icons" });
     eleventyConfig.addPassthroughCopy({ "src/css/": "./" });
     eleventyConfig.addWatchTarget('./src/js/');
-    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     
     return {
         markdownTemplateEngine: 'njk',
